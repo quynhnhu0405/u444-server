f617faed167399a928dff25ea51946a156115010
quynhu0405 <hoangnhu0405@gmail.com>
2025-07-28 22:12:09 +0700

    update : change schema feedback and remove api update note

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
-  tableNumber: { type: String, required: true },
   experience: { type: Number, min: 1, max: 5, required: true },
   flavor: { type: Number, min: 1, max: 5, required: true },
-  suggestion: { type: String },
-  note: { type: String },
   date: { type: Date, default: Date.now }
 });
 
